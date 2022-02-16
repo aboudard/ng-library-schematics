1d3113d8bb667d960b39464c8ae35b5b575d4ca2
Alain BOUDARD <aboudard@connectiv-it.com>
2022-02-16 10:13:32 +0100

    fix: reference the schematics

---
+++ projects/aboudard/ng-lib/schematics/ng-add/index.ts
@@ -0,0 +1,11 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+// Just return the tree
+export function ngAdd(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.log('info', `✅️ Running Schematics`);
+    context.addTask(new NodePackageInstallTask());
+    return tree;
+  };
+}
